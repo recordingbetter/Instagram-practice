48090560bfe0b15252345704806ba7f11f806166
Joe Donghee Cho <recordingbetter@gmail.com>
2017-06-12 20:21:14 +0900

    add __str__ to each class

--- django_app/post/models.py
+++ django_app/post/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+
 # from django.contrib.auth.models import User
 
 '''
@@ -12,16 +13,15 @@ member app
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL)
-    photo = models.ImageField(blank = True,
-                              upload_to = 'post',
-                              )
-    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL,
-                                        through = 'PostLike',
-                                        related_name = 'like_posts',
-                                        )
-    created_date = models.DateTimeField(auto_now_add = True)
-    modified_date = models.DateTimeField(auto_now = True)
-    tags = models.ManyToManyField('Tag', blank = True)
+    photo = models.ImageField(blank=True, upload_to='post', )
+    like_users = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        through='PostLike',
+        related_name='like_posts',
+    )
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+    tags = models.ManyToManyField('Tag', blank=True)
 
     def add_comment(self, user, content):
         """
@@ -30,7 +30,7 @@ class Post(models.Model):
         :param content:
         :return:
         """
-        return self.comment_set.create(author = user, content = content)
+        return self.comment_set.create(author=user, content=content)
         # return Comment.objects.create(post = self.post, author = user, content = content)
 
     def add_tag(self, tag_name):
@@ -40,12 +40,12 @@ class Post(models.Model):
         :return:
         """
         # tag가 있으면 True, 없으면 False + 객체 생성
-        tag, tag_created = Tag.objects.get_or_create(name = tag_name)
-        if not self.tags.filter(name = tag_name).exists():
+        tag, tag_created = Tag.objects.get_or_create(name=tag_name)
+        if not self.tags.filter(name=tag_name).exists():
             self.tags.add(tag)
 
-    # def __str__(self):
-    #     return
+    def __str__(self):
+        return '{} uploaded {} at {}.'.format(self.author, self.photo.name, self.created_date)
 
     @property
     def like_count(self):
@@ -53,17 +53,29 @@ class Post(models.Model):
         return self.like_users.count()
 
 
+class PostLike(models.Model):
+    post = models.ForeignKey(Post)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    # migrate 이후에는 필요없음...(이미 없는 테이블)
+    # class Meta:
+    #     db_table = 'post_post_like_users'
+
+    def __str__(self):
+        return '{} liked {} at {}.'.format(self.user, self.post, self.created_date)
+
+
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete = models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
     content = models.TextField()
     author = models.ForeignKey(settings.AUTH_USER_MODEL)
-    created_date = models.DateTimeField(auto_now_add = True)
-    modified_date = models.DateTimeField(auto_now = True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
     like_users = models.ManyToManyField(
         settings.AUTH_USER_MODEL,
-        through = 'CommentLike',
-        related_name = 'like_comments',
-    )
+        through='CommentLike',
+        related_name='like_comments', )
 
     def __str__(self):
         return '{} by {}'.format(self.content, self.author)
@@ -72,20 +84,14 @@ class Comment(models.Model):
 class CommentLike(models.Model):
     comment = models.ForeignKey(Comment)
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
-    created_date = models.DateTimeField(auto_now_add = True)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '{} liked {} at {}.'
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length = 50, blank = True)
+    name = models.CharField(max_length=50, blank=True)
 
     def __str__(self):
         return 'Tag({})'.format(self.name)
-
-
-class PostLike(models.Model):
-    post = models.ForeignKey(Post)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL)
-    created_date = models.DateTimeField(auto_now_add = True)
-    # migrate 이후에는 필요없음...(이미 없는 테이블)
-    # class Meta:
-    #     db_table = 'post_post_like_users'
