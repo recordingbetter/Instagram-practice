d74cb1c5091cfa4002511aa2ff8dd7517f05e7c6
Joe Donghee Cho <recordingbetter@gmail.com>
2017-06-15 11:42:34 +0900

    add member/login.html, view, urls

---
+++ django_app/member/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'member'
+
+urlpatterns = [
+    url(r'^login/$', views.login, name='login'),
+    ]
--- django_app/member/views.py
+++ django_app/member/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
+
+def login(request):
+    # member/login.html 생성
+    # username, password, button이 있는 html 생성
+    # POST 요청이 올 경우 좌측코드를 기반으로 로그인 완료 후 post_list로 이동
+    # 실패할 경우, HttpResponse로 'Login invalid!' 띄어주기
+
+    # member/urls.py 생성
+
+    if request.method == 'POST':
+        pass
+    else:
+        return render(request, 'member/login.html')
 
