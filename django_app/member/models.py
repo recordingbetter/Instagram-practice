3eeb18b45c7da6ef50df46da48bdb12fdbbf55f0
Joe Donghee Cho <recordingbetter@gmail.com>
2017-06-16 15:44:43 +0900

    make nickname field on member model

--- django_app/member/models.py
+++ django_app/member/models.py
@@ -1,7 +1,10 @@
-# from django.db import models
+from django.db import models
 # from django.contrib.auth.models import User
 from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    nickname = models.CharField(max_length=24, null=True, unique=True)
+
+    def __str__(self):
+        return self.nickname
