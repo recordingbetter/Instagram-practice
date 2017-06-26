49ed63df83b3d6c70c317f3f0384b9d8e57a0075
Joe Donghee Cho <recordingbetter@gmail.com>
2017-06-26 20:36:35 +0900

    save youtube search results to Video DB, render youtube_search.html with them

--- django_app/post/models.py
+++ django_app/post/models.py
@@ -1,6 +1,9 @@
 import re
 
+import requests
 from django.conf import settings
+from django.core.files import File
+from django.core.files.temp import NamedTemporaryFile
 from django.db import models
 # from django.contrib.auth.models import User
 from django.urls import reverse
@@ -148,5 +151,31 @@ class Tag(models.Model):
 class Video(models.Model):
     youtube_id = models.CharField(max_length=30)
     youtube_thumbnail = models.ImageField(upload_to='youtube_thumbnail')
+    youtube_thumbnail_url = models.CharField(max_length=50, null=True, blank=True)
     youtube_title = models.CharField(max_length=100)
     youtube_description = models.TextField(null=True, blank=True)
+
+    # def get_or_create_video(self, video_item):
+    #     video, video_create = self.get_or_create(
+    #         youtube_id=video_item['id']['videoId'],
+    #         defaults={
+    #             'youtube_title': video_item['snippet']['title'],
+    #             'youtube_thumbnails': video_item['snippet']['thumbnails']['high']['url'],
+    #             'youtube_description': video_item['snippet']['description']
+    #             }
+    #         )
+    #     if video_create:
+    #         url_thumbnail = video_item['snippet']['thumbnails']['high']['url']
+    #         p = re.compile(r'.*\.([^?]+)')
+    #         file_ext = re.search(p, url_thumbnail).group(1)
+    #         file_name = '{}.{}'.format(
+    #             video_item['id']['videoId'],
+    #             file_ext
+    #             )
+    #         temp_file = NamedTemporaryFile()
+    #         response = requests.get(url_thumbnail)
+    #         temp_file.write(response.content)
+    #         video.youtube_thumbnail.save(file_name, File(temp_file))
+    #     return video
+    #
+
--- django_app/post/views/youtube.py
+++ django_app/post/views/youtube.py
@@ -1,7 +1,12 @@
+import re
+
 import requests
-from django.shortcuts import render
+from django.core.files import File
+from django.core.files.temp import NamedTemporaryFile
+from django.shortcuts import render, redirect
 
 from config import settings
+from post.models import Video
 
 __all__ = [
     # 'youtube_search',
@@ -44,9 +49,39 @@ def youtube_search_save(request):
             'key': settings.YOUTUBE_KEY,
             }
         response = requests.get(url_api_search, params=search_params)
+        videos = response.json().get('items')
+        # 찾아진 결과를 DB에 저장
+        for video_item in videos:
+            video, video_create = Video.objects.get_or_create(
+                youtube_id=video_item['id']['videoId'],
+                defaults={
+                    'youtube_title': video_item['snippet']['title'],
+                    'youtube_thumbnail_url': video_item['snippet']['thumbnails']['high']['url'],
+                    'youtube_description': video_item['snippet']['description']
+                    }
+                )
+            if video_create:
+                url_thumbnail = video_item['snippet']['thumbnails']['high']['url']
+                p = re.compile(r'.*\.([^?]+)')
+                file_ext = re.search(p, url_thumbnail).group(1)
+                file_name = '{}.{}'.format(
+                    video_item['id']['videoId'],
+                    file_ext
+                    )
+                temp_file = NamedTemporaryFile()
+                response = requests.get(url_thumbnail)
+                temp_file.write(response.content)
+                video.youtube_thumbnail.save(file_name, File(temp_file))
+
+        search_result = Video.objects.filter(youtube_title__contains=q)
         context = {
-            'response': response.json(),
+            'search_result': search_result,
+            # 'videos': videos,
             }
     else:
         context = {}
     return render(request, 'post/youtube_search.html', context)
+
+
+# def youtube_search_post(request):
+
